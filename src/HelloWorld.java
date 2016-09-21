42330903a6e4ac0578383ee77bcffa3a60abf353
sd2017 <sd2017@walla.co.il>
2016-09-21 14:20:55 -0700

    docker redis java ping

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+import redis.clients.jedis.Jedis;
+
+public class HelloWorld
+{
+   public static void main (String args [])
+   {
+    Jedis jedis = new Jedis("redis");
+      while (true) {
+        System.out.println("Server is running: "+jedis.ping());
+	}
+  }
+}  
