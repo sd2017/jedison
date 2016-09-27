4f08c7b3d7e6456177e2477a86366205a9211869
sd2017 <sd2017@walla.co.il>
2016-09-27 07:06:43 -0700

    HelloWorld.java moved to maven src/java directory

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld
 {
    public static void main (String args [])
    {
+       System.out.println("in main before new jedis ");
     Jedis jedis = new Jedis("redis");
       while (true) {
         System.out.println("Server is running: "+jedis.ping());
