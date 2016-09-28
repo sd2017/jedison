fb0ac03ce39949289a971d0be7a948a5b1ba8847
sd2017 <sd2017@walla.co.il>
2016-09-28 09:14:59 -0700

    Hello world check for environment variable.

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,9 @@ FROM java:7
 COPY /src /src
 COPY /lib /lib
 COPY /target /target
-
+ENV  REDIS_SERVER redis
 #RUN jar cf target/jedison-0.2-SNAPSHOT.jar target/classes/
 #ENTRYPOINT  /bin/bash
 #RUN /bin/bash
 #RUN javac   -cp .:target/dependency/jedis-2.1.0.jar:target/jedison-0.2-SNAPSHOT.jar  -d . src/main/java/JedisTest.java src/main/java/HelloWorld.java
+RUN  ls -l
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,27 +1,47 @@
-import redis.clients.jedis.Jedis;
+    import redis.clients.jedis.Jedis;
 
-import java.net.URL;
-import java.net.URLClassLoader;
+    import java.net.URL;
+    import java.net.URLClassLoader;
 
-public class HelloWorld
-{
-   public static void main (String args [])
-   {
-       ClassLoader sysClassLoader = ClassLoader.getSystemClassLoader();
+    public class HelloWorld
+    {
+       public static void main (String args []) {
+           ClassLoader sysClassLoader = ClassLoader.getSystemClassLoader();
 
 
-       URL[] urls = ((URLClassLoader)sysClassLoader).getURLs();
+           URL[] urls = ((URLClassLoader) sysClassLoader).getURLs();
 
-       for(int i=0; i< urls.length; i++)
-       {
-           System.out.println("JAVA CP"+urls[i].getFile());
-       }
-       System.out.println("jedison 0.3 in main before new jedis ");
+           for (int i = 0; i < urls.length; i++) {
+               System.out.println("JAVA CP" + urls[i].getFile());
+           }
 
-    Jedis jedis = new Jedis("localhost_1");
-       System.out.println("Server is running 1 : "+jedis.ping());
-       //while (false) {
-       // System.out.println("Server is running: "+jedis.ping());
-	//}
-  }
-}  
+
+           String env="REDIS_SERVER";
+           String value = System.getenv(env);
+           if (value != null) {
+               System.out.format("$%s=%s%n",
+                       env, value);
+           } else {
+               System.out.format("$%s is"
+                       + " not assigned.%n", env);
+           }
+
+
+           String JEDIS_SERVER=null;
+               if (value !=null)
+               {
+                    JEDIS_SERVER = "redis";
+               }
+               else
+               {
+                    JEDIS_SERVER = "localhost";
+               }
+           System.out.println("jedison 0.3.6 in main before new jedis :"+JEDIS_SERVER);
+
+        Jedis jedis = new Jedis(JEDIS_SERVER);
+           System.out.println("Server is running 1 : "+jedis.ping());
+           //while (false) {
+           // System.out.println("Server is running: "+jedis.ping());
+        //}
+      }
+    }
